b69376ed66be219cd37f75270696c323e168406b
renato-Aspire-E5-574 <remontanari@live.com>
2022-05-23 10:56:35 -0300

    blade feed update order

--- resources/views/web/feed.blade.php
+++ resources/views/web/feed.blade.php
@@ -9,32 +9,6 @@
         <language>pt-br</language>
         <pubDate>{{ now() }}</pubDate>
 
-        @foreach($posts as $post)
-            <item>
-                <title><![CDATA[{{ $post->titulo }}]]></title>
-                <link>{{ url('blog/artigo/'.$post->slug) }}</link>
-                <image>{{ $post->cover() }}</image>
-                <description><![CDATA[{!! $post->getContentWebAttribute() !!}]]></description>
-                <category>{{ $post->categoriaObject->titulo }}</category>
-                <author><![CDATA[{{ $post->userObject->name }}]]></author>
-                <guid>{{ $post->id }}</guid>
-                <pubDate>{{ $post->created_at }}</pubDate>
-            </item>
-        @endforeach
-
-        @foreach($paginas as $pagina)
-            <item>
-                <title><![CDATA[{{ $pagina->titulo }}]]></title>
-                <link>{{ url('pagina/'.$pagina->slug) }}</link>
-                <image>{{ $pagina->cover() }}</image>
-                <description><![CDATA[{!! $pagina->getContentWebAttribute() !!}]]></description>
-                <category>{{ $pagina->categoriaObject->titulo }}</category>
-                <author><![CDATA[{{ $pagina->userObject->name }}]]></author>
-                <guid>{{ $pagina->id }}</guid>
-                <pubDate>{{ $pagina->created_at }}</pubDate>
-            </item>
-        @endforeach
-
         @foreach($noticias as $noticia)
             <item>
                 <title><![CDATA[{{ $noticia->titulo }}]]></title>
@@ -61,6 +35,32 @@
                     <pubDate>{{ $empresa->created_at }}</pubDate>
                 </item>
             @endforeach
-        @endif        
+        @endif   
+        
+        @foreach($posts as $post)
+            <item>
+                <title><![CDATA[{{ $post->titulo }}]]></title>
+                <link>{{ url('blog/artigo/'.$post->slug) }}</link>
+                <image>{{ $post->cover() }}</image>
+                <description><![CDATA[{!! $post->getContentWebAttribute() !!}]]></description>
+                <category>{{ $post->categoriaObject->titulo }}</category>
+                <author><![CDATA[{{ $post->userObject->name }}]]></author>
+                <guid>{{ $post->id }}</guid>
+                <pubDate>{{ $post->created_at }}</pubDate>
+            </item>
+        @endforeach
+
+        @foreach($paginas as $pagina)
+            <item>
+                <title><![CDATA[{{ $pagina->titulo }}]]></title>
+                <link>{{ url('pagina/'.$pagina->slug) }}</link>
+                <image>{{ $pagina->cover() }}</image>
+                <description><![CDATA[{!! $pagina->getContentWebAttribute() !!}]]></description>
+                <category>{{ $pagina->categoriaObject->titulo }}</category>
+                <author><![CDATA[{{ $pagina->userObject->name }}]]></author>
+                <guid>{{ $pagina->id }}</guid>
+                <pubDate>{{ $pagina->created_at }}</pubDate>
+            </item>
+        @endforeach
     </channel>
 </rss>
