06e21f2be39ed13e348950d7d022903982d62136
renato-Aspire-E5-574 <remontanari@live.com>
2022-06-24 00:56:28 -0300

    teste

--- config/filesystems.php
+++ config/filesystems.php
@@ -12,6 +12,8 @@ return [
     | based disks are available to your application. Just store away!
     |
     */
+
+    'default' => env('FILESYSTEM_DRIVER', 'local'),
     
     /*
     |--------------------------------------------------------------------------
