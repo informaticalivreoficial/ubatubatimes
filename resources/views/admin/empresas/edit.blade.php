9c0860fd82db04b0e04c733b8c4a31722332833c
renato-Aspire-E5-574 <remontanari@live.com>
2022-10-21 23:03:39 -0300

    update controller empresas

--- resources/views/admin/empresas/edit.blade.php
+++ resources/views/admin/empresas/edit.blade.php
@@ -171,7 +171,7 @@ $config = [
                                 <div class="form-group">
                                     <div class="thumb_user_admin">
                                         @php
-                                            if(!empty($empresa->logomarca) && \Illuminate\Support\Facades\Storage::exists($empresa->logomarca)){
+                                            if(!empty($empresa->logomarca) && env('AWS_PASTA') . \Illuminate\Support\Facades\Storage::exists($empresa->logomarca)){
                                                 $cover = $empresa->logoCover();
                                             } else {
                                                 $cover = url(asset('backend/assets/images/image.jpg'));
