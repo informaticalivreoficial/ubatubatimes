f5a92b8fe61b2f41978a18d7db669effefc464e0
informaticalivreoficial <renatomontanari@ymail.com>
2024-05-25 00:32:14 -0300

    fix rss

--- app/Http/Controllers/Web/RssFeedController.php
+++ app/Http/Controllers/Web/RssFeedController.php
@@ -13,7 +13,7 @@ class RssFeedController extends Controller
 {
     public function feed()
     {
-        $posts = Post::whereDate('created_at', Carbon::now()->subDays(5))
+        $posts = Post::whereDate('created_at', '>', Carbon::today()->subDays(8))
                 ->where('tipo', 'artigo')
                 ->postson()
                 ->limit(10)
@@ -23,7 +23,7 @@ class RssFeedController extends Controller
                 ->postson()
                 ->limit(10)
                 ->get();
-        $noticias = Post::whereDate('created_at', Carbon::now()->subDays(5))
+        $noticias = Post::whereDate('created_at', '>', Carbon::today()->subDays(6))
                 ->where('tipo', 'noticia')
                 ->postson()
                 ->limit(10)
